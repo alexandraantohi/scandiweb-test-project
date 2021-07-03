1152689bce29f3616c13634b6076547e3c5e39cf
alexandraantohi <73305226+alexandraantohi@users.noreply.github.com>
2021-07-03 15:18:57 +0300

    better js

--- switcher.js
+++ switcher.js
@@ -2,55 +2,57 @@ let dvd = document.getElementById('dvdDetails');
 
 let furniture = document.getElementById('furnitureDetails');
 furniture.style.display = 'none';
+
 let book = document.getElementById('bookDetails');
 book.style.display = 'none';
 
 let selectElement = document.getElementById('productType');
 selectElement.addEventListener('change', () => {
-    
+    hideAll();
+    const actions = {
+        "book": "showBook",
+        "dvd": "showDvd",
+        "furniture": "showFurniture"
+    }
+    const fn = window[actions[selectElement.value]];
+    fn();
+   
+})
+
+const hideAll = () => {
+
     dvd.style.display = 'none';
     furniture.style.display = 'none';
     book.style.display = 'none';
 
     list = document.getElementsByClassName("product-details");
-    for(let i = 0; i < list.length; i++){
+    for (let i = 0; i < list.length; i++) {
         list[i].required = false;
     }
+}
 
-    switch(selectElement.value) {
-        case 'dvd':
-            dvd.style.display = 'inline';
-            document.getElementById('size').required = true;
-            break;
-        case 'furniture':
-            furniture.style.display = 'inline';
-            document.getElementById('height').required = true;
-            document.getElementById('width').required = true;
-            document.getElementById('length').required = true;
-            break;
-        case 'book':
-            book.style.display = 'inline';
-            document.getElementById('weight').required = true;
-            break;
-    }
-})
+const showDvd = () => {
+    dvd.style.display = 'inline';
+    document.getElementById('size').required = true;
+}
 
+const showFurniture = () => {
+    furniture.style.display = 'inline';
+    document.getElementById('height').required = true;
+    document.getElementById('width').required = true;
+    document.getElementById('length').required = true;
+}
 
-// const validateForm = () => {
-    
-//     let form = document.forms['product_form'];
-//     if(!form['sku'].value.length) {
-//         console.log('nu exista');
-//     }
-//     return false;
-
-// }
+const showBook = () => {
+    book.style.display = 'inline';
+    document.getElementById('weight').required = true;
+}
 
 
 const check = input => {
     if (input.value <= 0) {
-      input.setCustomValidity('The number must be greater than 0');
+        input.setCustomValidity('The number must be greater than 0');
     } else {
-      input.setCustomValidity('');
+        input.setCustomValidity('');
     }
-  }
+}
